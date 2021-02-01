b659f6fd44a73f0b01225cc494dea20a8ded0b81
Asterisk Azurain <qysx32_kitty@yeah.net>
2021-02-02 00:28:03 +0800

    create solution

---
+++ RepairMS/Article.cs
@@ -0,0 +1,9 @@
+﻿namespace RepairMS
+{
+    public class Article
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
